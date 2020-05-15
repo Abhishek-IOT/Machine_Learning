fe387866e8c71d7c1fc731ac86194df228080f8d
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-15 13:40:57 +0530

    taking the linear Regression as the testing model.

--- Machine Learning Algos/Linear Regression.py
+++ Machine Learning Algos/Linear Regression.py
@@ -1,7 +1,10 @@
 import numpy as np
-from sklearn import datasets
+from sklearn import datasets, linear_model
 
 daibetic = datasets.load_diabetes()
 X = daibetic.data[:, np.newaxis, 2]
 X_train = X[:30]
 X_test = X[:-20]
+Y_train = daibetic.target[:30]
+Y_test = daibetic.target[:-20]
+model = linear_model.LinearRegression()
