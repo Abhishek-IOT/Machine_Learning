79e6dfb4b24d2c623f8955a433a244f2c7315a4d
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-15 13:51:24 +0530

    for calculating the error and intercept and cofficient

--- Machine Learning Algos/Linear Regression.py
+++ Machine Learning Algos/Linear Regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error
 
 daibetic = datasets.load_diabetes()
 X = daibetic.data[:, np.newaxis, 2]
@@ -10,3 +11,6 @@ Y_test = daibetic.target[:-20]
 model = linear_model.LinearRegression()
 model.fit(X_train, Y_train)
 Y_predict = model.predict(X_test)
+print("Error is:", mean_squared_error(Y_test, Y_predict))
+print("Weights ", model.coef_)
+print("Intercept", model.intercept_)
