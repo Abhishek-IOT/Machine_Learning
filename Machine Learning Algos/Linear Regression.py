f7675bb8887fd66452dae7286d108a5bbd9e9ab3
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-15 13:37:32 +0530

    taking one feature and doing it for the test

--- Machine Learning Algos/Linear Regression.py
+++ Machine Learning Algos/Linear Regression.py
@@ -1,5 +1,7 @@
-import matplotlib as plt
 import numpy as np
-from sklearn import datasets,linear_model
-daibetic=datasets.load_diabetes()
-print(daibetic.data)
+from sklearn import datasets
+
+daibetic = datasets.load_diabetes()
+X = daibetic.data[:, np.newaxis, 2]
+X_train = X[:30]
+X_test = X[:-20]
